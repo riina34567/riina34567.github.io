c9ba41b14419bdba62457282b0aed283f7b81039
riina34567 <riinasasaki456@gmail.com>
2024-06-18 03:25:16 +1000

    Revert "finsih java A3"
    This reverts commit 70ad22d862f20ca520d41aba0d3b1d047edd0c5e.

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,15 +1,3 @@
-const banner = document.querySelector("#banner");
-console.log(banner);
-
-const close = document.querySelector("#close");
-console.log(close);
-
-const crossImg = document.querySelector("#crossimg");
-console.log(crossImg);
-
-document.querySelector(".banner").addEventListener("click", function () {
-  this.closest(".banner").style.display = "none";
+document.querySelector("#banner-close").addEventListener("click", function () {
+  this.closest("banner").style.display = "none";
 });
-
-// For the javascript, I just added a simple closing banner button for more user interaction and so the
-// users are informed that they can hover over the countries to see more information.
