1262a2c71eb8f795cd77035b68cfca393ee1905d
riina34567 <riinasasaki456@gmail.com>
2024-06-19 20:15:47 +1000

    finish 2nd A3

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,3 +1,12 @@
-document.querySelector("#banner-close").addEventListener("click", function () {
-  this.closest("banner").style.display = "none";
+const banner = document.querySelector("#banner");
+console.log(banner);
+
+const close = document.querySelector("#close");
+console.log(banner);
+
+const crossImg = document.querySelector("#crossimg");
+console.log(crossImg);
+
+document.querySelector(".banner-close").addEventListener("click", function () {
+  this.closest(".banner").style.display = "none";
 });
